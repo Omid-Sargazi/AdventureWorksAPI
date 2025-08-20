d1d995f5c87ac7dec5470bf623890937d7596f04
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-20 19:27:39 +0330

    Bubble Sort in C#

---
+++ DataStructure/Sorting/SortingInCSharp.cs
@@ -0,0 +1,41 @@
+namespace DataStructure.Sorting
+{
+    public class SortingInCSharp
+    {
+        public static void Bubble(int[] arr)
+        {
+            for (int start = arr.Length-1; start >= 0; start--)
+            {
+                bool swapped = false;
+                for (int j = 0; j < start; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                        swapped = true;
+                    }
+
+                }
+                    if (!swapped) break;
+            }
+
+            Console.WriteLine(string.Join(",", arr));
+        }
+
+
+        public static void Insertion(int[] arr)
+        {
+
+        }
+
+        public static void Selection(int[] arr)
+        {
+
+        }
+
+        public static void Merge(int[] arr)
+        {
+
+        }
+    }
+}
