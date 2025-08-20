a9a1d8b811e86d1613235def25f5c14e6cac21bf
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-20 10:20:22 +0330

    Selection Sort

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -4,4 +4,5 @@ using DataStructure.Sorting;
 Console.WriteLine("Hello, World!");
 int[] arr = new int[] { 70, -70, 1, 4, 50, 78, 87, 88, 99 };
 int[] arr2 = new int[] {0,0,0 };
-BubbleSort.Run(arr2);
+// BubbleSort.Run(arr2);
+SelectionSort.Run(arr);
--- DataStructure/Sorting/Sorting.cs
+++ DataStructure/Sorting/Sorting.cs
@@ -24,4 +24,30 @@ namespace DataStructure.Sorting
             Console.WriteLine(string.Join(",", arr));
         }
     }
+
+
+    public class SelectionSort
+    {
+        public static void Run(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int minIndex = i;
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (i != minIndex)
+                {
+                    (arr[i], arr[minIndex]) = (arr[minIndex], arr[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(",", arr));
+        }
+    }
 }
