63a7afc4875cdeb4ba1f96aed51d5e82f3593c8a
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-20 10:14:51 +0330

    Bubble Sorting

---
+++ DataStructure/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructure.Sorting;
+
+Console.WriteLine("Hello, World!");
+int[] arr = new int[] { 70, -70, 1, 4, 50, 78, 87, 88, 99 };
+int[] arr2 = new int[] {0,0,0 };
+BubbleSort.Run(arr2);
---
+++ DataStructure/Sorting/Sorting.cs
@@ -0,0 +1,27 @@
+namespace DataStructure.Sorting
+{
+    public class BubbleSort
+    {
+        public static void Run(int[] arr)
+        {
+            for (int start = arr.Length - 1; start >= 0; start--)
+            {
+                bool swapped = false;
+                for (int j = 0; j < start - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                    }
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(string.Join(",", arr));
+        }
+    }
+}
