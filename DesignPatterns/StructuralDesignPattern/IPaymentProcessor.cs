1ee2f1b9653bf6045330cb26b43c5ef02fb50712
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-30 18:11:09 +0330

    Implements for Concrete Implementor

--- DesignPatterns/StructuralDesignPattern/IPaymentProcessor.cs
+++ DesignPatterns/StructuralDesignPattern/IPaymentProcessor.cs
@@ -5,5 +5,27 @@ namespace DesignPatterns.StructuralDesignPattern
         void ProcessPayment(decimal amount);
     }
 
-    
+    public class BankAPIPaymentProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"پرداخت مبلغ {amount} تومان از طریق API بانک با موفقیت انجام شد.");
+        }
+    }
+
+    public class ThirdPartyPaymentProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"پرداخت مبلغ {amount} تومان از طریق درگاه پرداخت شخص ثالث با موفقیت انجام شد.");
+        }
+    }
+
+    public class TestPaymentProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+             Console.WriteLine($"(TEST) پرداخت مبلغ {amount} تومان در محیط آزمایشی با موفقیت شبیه‌سازی شد.");
+        }
+    }
 }
