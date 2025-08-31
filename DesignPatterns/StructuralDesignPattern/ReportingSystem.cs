c28426ed6a6412938b3ccbbc350af05435fc850b
Omid-Sargazi <om.sargazi@gmail.com>
2025-08-31 23:25:55 +0330

    Abstraction classes

--- DesignPatterns/StructuralDesignPattern/ReportingSystem.cs
+++ DesignPatterns/StructuralDesignPattern/ReportingSystem.cs
@@ -28,4 +28,53 @@ namespace DesignPatterns.StructuralDesignPattern
             throw new NotImplementedException();
         }
     }
+
+    public abstract class Report
+    {
+        protected readonly IExportFormat _exportFormat;
+
+        public Report(IExportFormat exportFormat)
+        {
+            _exportFormat = exportFormat;
+        }
+
+        public abstract void Generate();
+    }
+
+    public class SalesReport : Report
+    {
+        private readonly string _salesData;
+        public SalesReport(IExportFormat exportFormat, string salesData) : base(exportFormat)
+        {
+            _salesData = salesData;
+        }
+
+        public override void Generate()
+        {
+            Console.WriteLine("جمع‌آوری داده‌های فروش...");
+            string reportContent = $"گزارش فروش: {_salesData}";
+            _exportFormat.GenerateFile(reportContent, "SalesReport");
+        }
+    }
+
+    public class InventoryReport : Report
+    {
+        private readonly string _inventoryData;
+
+        public InventoryReport(IExportFormat exportFormat, string inventoryData)
+            : base(exportFormat)
+        {
+            _inventoryData = inventoryData;
+        }
+
+        public override void Generate()
+        {
+            // منطق خاص گزارش موجودی
+            Console.WriteLine("بررسی موجودی انبار...");
+            string reportContent = $"گزارش موجودی: {_inventoryData}";
+
+            _exportFormat.GenerateFile(reportContent, "InventoryReport");
+        }
+    }
+
 }
