fb2b62320563a1fa912d58df52dd03cbd7e7cc55
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-02 11:59:18 +0330

    Add three quadratic sorting algorithms: Bubble Sort, Selection Sort, Insertion Sort - All have O(n^2) time complexity - Include optimizations like early termination - Support both ascending and descending orders

--- Sorting/SortingAlgorithems/Sorting.cs
+++ Sorting/SortingAlgorithems/Sorting.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+using System.Runtime.Intrinsics.Arm;
 using System.Security.AccessControl;
 
 namespace Sorting.SortingAlgorithems
@@ -22,7 +24,7 @@ namespace Sorting.SortingAlgorithems
             }
         }
 
-        private static void Heapify(int[] arr, int i,int n)
+        private static void Heapify(int[] arr, int i, int n)
         {
             int left = 2 * i + 1;
             int right = 2 * i + 2;
@@ -41,8 +43,73 @@ namespace Sorting.SortingAlgorithems
             if (largest != i)
             {
                 (arr[i], arr[largest]) = (arr[largest], arr[i]);
-                Heapify(arr, largest,n);
+                Heapify(arr, largest, n);
             }
+        }
+
+        public static void Bubble(int[] arr)
+        {
+            for (int start = arr.Length - 1; start >= 0; start--)
+            {
+                bool swapped = false;
+                for (int j = 0; j < start; j++)
+                {
+                    if (arr[j] < arr[j + 1])
+                    {
+                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                        swapped = true;
+                    }
+                }
+
+
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Bubble sort: {string.Join(",", arr)}");
+        }
+
+        public static void Selection(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                if (i != minIndex)
+                {
+                    (arr[i], arr[minIndex]) = (arr[minIndex], arr[i]);
+                }
+            }
+
+
+            Console.WriteLine($"Selection sort: {string.Join(",", arr)}");
+
+        }
+
+        public static void Insertion(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int j = i - 1;
+                int current = arr[i];
+                while (j >= 0 && arr[j] > current)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = current;
+            }
+            Console.WriteLine($"Insertion sort: {string.Join(",", arr)}");
+
         }
     }
 }
