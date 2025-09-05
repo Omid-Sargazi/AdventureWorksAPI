187c404017de95d1755fca2a737061c44d09d9a9
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-05 11:47:40 +0330

    Create Emplyee with interface way

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -2,4 +2,5 @@
 using DesignPatterns.StructuralDesignPattern;
 
 Console.WriteLine("Hello, World! from Design Pattern");
-ClientComponent.Run();
+// ClientComponent.Run();
+ClienEmployee.Run();
--- DesignPatterns/StructuralDesignPattern/EmployeeComponent.cs
+++ DesignPatterns/StructuralDesignPattern/EmployeeComponent.cs
@@ -21,4 +21,55 @@ namespace DesignPatterns.StructuralDesignPattern
 
         public abstract void Display(int depth = 0);
     }
+
+    public interface IEmployeeComponent
+    {
+        string Name { get; }
+        string Position { get; }
+
+        decimal GetSalary();
+        void Display(int depth = 0);
+    }
+
+    public class Employee : IEmployeeComponent
+    {
+        public string Name { get; }
+
+        public string Position { get; }
+
+        public decimal Salary;
+
+        public Employee(string name, string position, decimal salary)
+        {
+            Name = name;
+            Position = position;
+            Salary = salary;
+        }
+
+        public void Display(int depth = 0)
+        {
+            Console.WriteLine($"{new string('-', depth)} {Position}: {Name}, Salary = {Salary}");
+
+        }
+
+        public decimal GetSalary()
+        {
+            return Salary;
+        }
+    }
+
+
+    public class ClienEmployee
+    {
+        public static void Run()
+        {
+            IEmployeeComponent emp1 = new Employee("omid", "Developer", 5000);
+            IEmployeeComponent emp2 = new Employee("Saeed", " PHP Developer", 5000);
+
+            emp1.Display();
+            emp2.Display();
+
+            Console.WriteLine($"Total Salary = {emp1.GetSalary() + emp2.GetSalary()}");
+        }
+    }
 }
