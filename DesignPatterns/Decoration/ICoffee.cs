d2b39c108939c4ca7d536b18ec1e04c0a64ce869
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-11 13:37:20 +0330

    Add other features to the main object.

--- DesignPatterns/Decoration/ICoffee.cs
+++ DesignPatterns/Decoration/ICoffee.cs
@@ -24,13 +24,47 @@ namespace DesignPatterns.Decoration
         protected readonly ICoffee _coffee;
         public CoffeeDecorator(ICoffee coffee)
         {
-            _coffee =coffee;
+            _coffee = coffee;
         }
         public virtual double GetCost() => _coffee.GetCost();
 
         public virtual string GetDescription()
         {
             return _coffee.GetDescription();
+        }
+    }
+
+    public class MilkDecorator : CoffeeDecorator
+    {
+        public MilkDecorator(ICoffee coffee) : base(coffee)
+        {
+        }
+
+        public override string GetDescription()
+        {
+            return base.GetDescription() + "milk";
+        }
+
+        public override double GetCost()
+        {
+            return base.GetCost() + 0.6;
+        }
+    }
+
+    public class SugarDecorator : CoffeeDecorator
+    {
+        public SugarDecorator(ICoffee coffee) : base(coffee)
+        {
+        }
+
+        public override string GetDescription()
+        {
+            return base.GetDescription() + "Sugar";
+        }
+
+        public override double GetCost()
+        {
+            return base.GetCost() + 0.2;
         }
     }
 }
