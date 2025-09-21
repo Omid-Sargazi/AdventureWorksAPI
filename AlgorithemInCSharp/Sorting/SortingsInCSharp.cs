970d68e0957dc82fed3f2fe3257409fbfd9a0e7e
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-21 17:39:54 +0330

    Selection Sort In C#

--- AlgorithemInCSharp/Sorting/SortingsInCSharp.cs
+++ AlgorithemInCSharp/Sorting/SortingsInCSharp.cs
@@ -20,5 +20,22 @@ namespace AlgorithemInCSharp.Sorting
 
             Console.WriteLine($"{string.Join(",", arr)}");
         }
+
+        public static void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                (arr[i], arr[minIndex]) = (arr[minIndex], arr[i]);
+            }
+            Console.WriteLine($"{string.Join(",", arr)}");
+        }
     } 
 }
