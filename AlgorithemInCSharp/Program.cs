42b7eb9115c3bd77ef6930a626b35abf80ad4ff6
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-21 17:32:56 +0330

    Bubble Sort in CSharp

---
+++ AlgorithemInCSharp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AlgorithemInCSharp.Sorting;
+
+Console.WriteLine("Hello, World!");
+int[] arr1 = new int[] { 5, 4, 3, 2, 1 };
+SortingsInCSharp.Buuble(arr1);
