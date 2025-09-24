3bb0730ff2dd57759e82704f30c7543d9d46871f
Omid-Sargazi <om.sargazi@gmail.com>
2025-09-24 08:54:09 +0330

    .

--- AlgorithemAndDataStructure/Program.cs
+++ AlgorithemAndDataStructure/Program.cs
@@ -16,4 +16,21 @@ int target2 = 10;
 // var res = TwoSum.Run(arr, target);
 // Console.WriteLine($"{string.Join(",", res)}");
 // TwoSum.RunTwoSumWithDictionary(arr, target);
-TwoSum.RunTwoSumWithDictionary2(arr2, target2);
+// TwoSum.RunTwoSumWithDictionary2(arr2, target2);
+
+
+Dictionary<string, int> dict1 = new Dictionary<string, int>()
+{
+    ["Omid"] = 1,
+    ["Saeed"] = 2,
+    ["Vahid"] = 3
+};
+
+if (dict1.TryGetValue("Omid", out int value))
+{
+    Console.WriteLine(value);
+}
+else
+{
+    Console.WriteLine("Not Found");
+}
