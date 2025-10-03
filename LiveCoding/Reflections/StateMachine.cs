b157a4c9b20382fdd46e8fa9d397b9ef76d17193
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-03 11:55:07 +0330

    State Machine

---
+++ LiveCoding/Reflections/StateMachine.cs
@@ -0,0 +1,45 @@
+namespace LiveCoding.Reflections
+{
+    public enum TrafficLightState
+    {
+        Red,
+        Yellow,
+        Green,
+    }
+    public class TrafficLight
+    {
+        private TrafficLightState _currentState = TrafficLightState.Red;
+
+        public void ChangeState()
+        {
+            switch (_currentState)
+            {
+                case TrafficLightState.Red:
+                    _currentState = TrafficLightState.Green;
+                    Console.WriteLine("Move");
+                    break;
+
+                case TrafficLightState.Green:
+                    _currentState = TrafficLightState.Yellow;
+                    Console.WriteLine("Caution");
+                    break;
+
+                case TrafficLightState.Yellow:
+                    _currentState = TrafficLightState.Red;
+                    Console.WriteLine("Stop");
+                    break;
+            }
+        }
+    }
+
+    public class ClientStateMachin
+    {
+        public static void Run()
+        {
+            var trffic = new TrafficLight();
+            trffic.ChangeState();
+            trffic.ChangeState();
+            trffic.ChangeState();
+        }
+    }
+}
