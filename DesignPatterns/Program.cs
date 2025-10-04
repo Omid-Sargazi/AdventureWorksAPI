1d85532bc62df918abcb6b2321084cdbc63104c1
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-04 23:40:02 +0330

    Binary Search Tree

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,33 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-using DesignPatterns.StructuralDesignPattern;
-using DesignPatterns.StructuralDesignPattern2;
+﻿// // See https://aka.ms/new-console-template for more information
+// using DesignPatterns.StructuralDesignPattern;
+// using DesignPatterns.StructuralDesignPattern2;
 
-Console.WriteLine("Hello, World! from Design Pattern");
-// ClientComponent.Run();
-// ClienEmployee.Run();
-// ClientEmployeeLeaf.Run();
+// Console.WriteLine("Hello, World! from Design Pattern");
+// // ClientComponent.Run();
+// // ClienEmployee.Run();
+// // ClientEmployeeLeaf.Run();
 
-SimpleFile file1 = new SimpleFile("f1.txt", 20);
-SimpleFile file2 = new SimpleFile("f2.txt", 30);
-SimpleFile img1 = new SimpleFile("i1.img", 300);
-SimpleFile img2 = new SimpleFile("i2.img", 200);
+// SimpleFile file1 = new SimpleFile("f1.txt", 20);
+// SimpleFile file2 = new SimpleFile("f2.txt", 30);
+// SimpleFile img1 = new SimpleFile("i1.img", 300);
+// SimpleFile img2 = new SimpleFile("i2.img", 200);
 
-Folder folder1 = new Folder("texts");
-Folder folder2 = new Folder("images");
+// Folder folder1 = new Folder("texts");
+// Folder folder2 = new Folder("images");
 
-Folder Mainfolder = new Folder("Main");
-Folder Rootfolder = new Folder("Root");
+// Folder Mainfolder = new Folder("Main");
+// Folder Rootfolder = new Folder("Root");
 
-folder1.AddComponent(file1);
-folder1.AddComponent(file2);
+// folder1.AddComponent(file1);
+// folder1.AddComponent(file2);
 
-folder2.AddComponent(img1);
-folder2.AddComponent(img2);
-Mainfolder.AddComponent(folder1);
-Mainfolder.AddComponent(folder2);
-Rootfolder.AddComponent(Mainfolder);
-Rootfolder.Display();
-Console.WriteLine(Rootfolder.GetSize());
+// folder2.AddComponent(img1);
+// folder2.AddComponent(img2);
+// Mainfolder.AddComponent(folder1);
+// Mainfolder.AddComponent(folder2);
+// Rootfolder.AddComponent(Mainfolder);
+// Rootfolder.Display();
+// Console.WriteLine(Rootfolder.GetSize());
 
 
-ClientReport.Run();
+// ClientReport.Run();
+
+
+using DesignPatterns.Trees;
+
+ClientBST.Run();
