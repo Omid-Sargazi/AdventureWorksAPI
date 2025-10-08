3dcefe6c776331d4c7633a1f98cce9517dd5c901
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-08 10:45:36 +0330

    Create HeapMax structure

--- MiniDiDemo/DataStructures/LinkedList2.cs
+++ MiniDiDemo/DataStructures/LinkedList2.cs
@@ -88,6 +88,110 @@ namespace MiniDiDemo.DataStructures
             l1.Reverse();
             l1.Print();
             //======================
+
+            //======================
+            BST bST = new BST();
+            bST.Add(17);
+            bST.Add(10);
+            bST.Add(11);
+            bST.Add(9);
+            bST.Add(8);
+            bST.Add(18);
+            bST.Add(16);
+            bST.Add(19);
+            bST.InOrder();
+            //======================
+
+
+        }
+    }
+
+    public class TreeNode
+    {
+        public int Data { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+
+        public TreeNode(int data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+
+    public class BST
+    {
+        private TreeNode _root;
+        public BST() { }
+        public BST(TreeNode root)
+        {
+            _root = root;
+        }
+
+        public void Add(int value)
+        {
+            _root = AddTree(_root, value);
+        }
+
+        private TreeNode AddTree(TreeNode root, int value)
+        {
+            if (root == null)
+            {
+                return new TreeNode(value);
+            }
+
+            if (root.Data > value)
+            {
+                root.Left = AddTree(root.Left, value);
+            }
+            if (root.Data < value)
+            {
+                root.Right = AddTree(root.Right, value);
+            }
+
+            return root;
+        }
+
+        public void InOrder()
+        {
+            InOrderTree(_root);
+        }
+
+        private void InOrderTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                // Console.WriteLine($"List is empty");
+                return;
+            }
+
+            InOrderTree(root.Left);
+            Console.WriteLine(root.Data);
+            InOrderTree(root.Right);
+
+        }
+    }
+
+    public class MaxHeap
+    {
+        private List<int> _elements = new();
+
+        private int GetLeftChildIndex(int i) => 2 * i + 1;
+        private int GetRightChildIndex(int i) => 2 * i + 2;
+        private int GetParentIndex(int i) => (i - 1) / 2;
+
+        private bool HasLeftChild(int i) => GetLeftChildIndex(i) < _elements.Count;
+        private bool HasRightChild(int i) => GetRightChildIndex(i) < _elements.Count;
+        private bool HasParent(int i) => i > 0;
+
+        private int LeftChild(int i) => _elements[GetLeftChildIndex(i)];
+        private int RightChild(int i) => _elements[GetRightChildIndex(i)];
+        private int Parent(int i) => _elements[GetParentIndex(i)];
+
+        private void Swap(int i, int j)
+        {
+            (_elements[i], _elements[j]) = (_elements[j], _elements[i]);
         }
     }
 }
