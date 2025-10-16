15d5a6ed66a082488d96c2854304f528deeb4985
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-16 13:38:04 +0330

    Usage of it

--- RelectionProblems02/Program.cs
+++ RelectionProblems02/Program.cs
@@ -1,14 +1,19 @@
+using System.Reflection;
+using RelectionProblems02.Problems;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddRepositories(Assembly.GetExecutingAssembly());
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
 
 app.UseHttpsRedirection();
 
