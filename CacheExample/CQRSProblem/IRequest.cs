ed9f18fa1b69b1dac009c468e89a8538fd7c6c95
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-17 10:39:15 +0330

    Mediator Pattern

---
+++ CacheExample/CQRSProblem/IRequest.cs
@@ -0,0 +1,46 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CacheExample.CQRSProblem
+{
+    public interface IRequest<TResult>
+    {
+
+    }
+
+    public interface IRequetHandler<TRequest, TResult> where TRequest : IRequest<TResult>
+    {
+        TResult Handle(TRequest request);
+    }
+
+    public class UserCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+
+    public class UserCommandHandler : IRequetHandler<UserCommand, bool>
+    {
+        public bool Handle(UserCommand request)
+        {
+            Console.WriteLine($"User created with Id {request.Id}");
+            return true;
+        }
+    }
+
+    public class Mediator
+    {
+        private  readonly IServiceProvider _serviceProvider;
+        public Mediator(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public TResult Send<TRequest,TResult>(TRequest request) where TRequest:IRequest<TResult>
+        {
+            var handler = _serviceProvider.GetService<IRequetHandler<TRequest, TResult>>();
+            return handler.Handle(request);
+        }
+
+             
+    }
+
+}
