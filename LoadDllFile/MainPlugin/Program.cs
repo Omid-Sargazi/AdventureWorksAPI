877831dccd67f435d8ee7a11bb79b0915efc1e91
Omid-Sargazi <om.sargazi@gmail.com>
2025-10-19 10:20:13 +0330

    load files of appsetting.json and create a plugin

--- LoadDllFile/MainPlugin/Program.cs
+++ LoadDllFile/MainPlugin/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Security.AccessControl;
 using Microsoft.Extensions.Configuration;
 
 public class Program
@@ -10,10 +11,47 @@ public class Program
 
         var assemblyPath = config["PluginSettings:AssemblyPath"];
         var className = config["PluginSettings:ClassName"];
-        var methodName = config["PluginSettings:Methodname"];
+        var methodName = config["PluginSettings:MethodName"];
 
         Console.WriteLine($"Assembly:{assemblyPath}");
         Console.WriteLine($"Class:{className}");
         Console.WriteLine($"method:{methodName}");
+
+        string pluginFolder = Path.Combine(Directory.GetCurrentDirectory(), "plugins");
+        if (!Directory.Exists(pluginFolder))
+        {
+            Directory.CreateDirectory(pluginFolder);
+        }
+
+        var pluginFiles = Directory.GetFiles(pluginFolder, "*.dll");
+        
+        foreach(var file in pluginFiles)
+        {
+            Console.WriteLine($"Loading...{Path.GetFileName(file)}");
+
+            Assembly assembly = Assembly.LoadFrom(file);
+
+            var pluginTypes = assembly.GetTypes().Where(t => t.FullName == className && !t.IsInterface && !t.IsAbstract).ToList();
+
+            foreach (var type in pluginTypes)
+            {
+                Console.WriteLine($"Found type: {type}");
+
+                var insta = Activator.CreateInstance(type);
+                Console.WriteLine($"Found type: {insta}");
+
+                var method = type.GetMethod(methodName, BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance);
+                 if (method == null)
+                    {
+                        Console.WriteLine($"❌ Method {methodName} not found in {type.FullName}");
+                        continue;
+                    }
+                    Console.WriteLine(method);
+                method.Invoke(null, null);
+                
+            }
+            
+        }
+
     }
 }
---
+++ LoadDllFile/SubPlugin/HelloService.cs
@@ -0,0 +1,10 @@
+namespace SubPlugin
+{
+    public class HelloService
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
