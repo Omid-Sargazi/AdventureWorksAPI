96580af24f64d9a889f84474bbeac751aa5a7a7b
Omid-Sargazi <om.sargazi@gmail.com>
2025-12-09 19:19:57 +0330

    Abstarct Class In Bridge Pattern

--- DesignPatternsInCSharp/StructuraDesignPattern/IEmailSender.cs
+++ DesignPatternsInCSharp/StructuraDesignPattern/IEmailSender.cs
@@ -4,4 +4,12 @@ namespace DesignPatternsInCSharp.StructuraDesignPattern
     {
         void SendEmail(string subject, string body);
     }
+
+    public abstract class Email
+    {
+        public IEmailSender MesaageSender{get;set;}
+        public string Subject {get;set;}
+        public string Body {get;set;}
+        public abstract void Send();
+    }
 }
