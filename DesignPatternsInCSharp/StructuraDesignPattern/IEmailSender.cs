25ec96902b36b95c21fa5ca8312e9f2819582109
Omid-Sargazi <om.sargazi@gmail.com>
2025-12-09 19:27:44 +0330

    implement diffrent ways for use in client

--- DesignPatternsInCSharp/StructuraDesignPattern/IEmailSender.cs
+++ DesignPatternsInCSharp/StructuraDesignPattern/IEmailSender.cs
@@ -41,4 +41,28 @@ namespace DesignPatternsInCSharp.StructuraDesignPattern
         }
     }
 
+
+    public class WebServiceEmailSender : IEmailSender
+    {
+        public void SendEmail(string subject, string body)
+        {
+            Console.WriteLine("Sending Email using WebService:\n{0}\n{1}\n", subject, body); 
+        }
+    }
+
+    public class WCFEmailSender : IEmailSender
+    {
+        public void SendEmail(string subject, string body)
+        {
+            Console.WriteLine("Sending Email using WCF:\n{0}\n{1}\n", subject, body);
+        }
+    }
+
+    public class WebAPIEmailSender : IEmailSender
+    {
+        public void SendEmail(string subject, string body)
+        {
+            Console.WriteLine("Sending Email using Web API:\n{0}\n{1}\n", subject, body); 
+        }
+    }
 }
