ad55a0ee3decb4a4c456aaa27161e80c239b5330
Omid-Sargazi <om.sargazi@gmail.com>
2025-12-09 19:24:23 +0330

    Create diffrent way for snding Emails

--- DesignPatternsInCSharp/StructuraDesignPattern/IEmailSender.cs
+++ DesignPatternsInCSharp/StructuraDesignPattern/IEmailSender.cs
@@ -12,4 +12,33 @@ namespace DesignPatternsInCSharp.StructuraDesignPattern
         public string Body {get;set;}
         public abstract void Send();
     }
+
+    public class SystemEmail : Email
+    {
+        public override void Send()
+        {
+            string emailSubject = string.Format("Subject: {0} from System", Subject);
+ 
+            string emailBody = string.Format("Email Body:\n{0}", Body);
+
+            MesaageSender.SendEmail(emailSubject,emailBody);
+        }
+    }
+
+    public class UserEmail : Email
+    {
+        public override void Send()
+        {
+            string emailSubject = string.Format("Subject: {0} from User", Subject);
+ 
+  
+ 
+       string emailBody = string.Format("Email Body:\n{0}", Body);
+ 
+  
+ 
+         MesaageSender.SendEmail(emailSubject, emailBody);
+        }
+    }
+
 }
