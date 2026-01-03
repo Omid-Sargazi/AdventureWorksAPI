f4a6dcc598ab21987fabf21899ccefb353c88237
Omid-Sargazi <om.sargazi@gmail.com>
2026-01-03 20:59:04 +0330

    Each style has consistent material and design characteristics - Create abstract factory interface IFurnitureFactory

--- ProblemsInLINQINCSharp/AbstarctFactoryPattern/IChair.cs
+++ ProblemsInLINQINCSharp/AbstarctFactoryPattern/IChair.cs
@@ -94,4 +94,11 @@ namespace ProblemsInLINQINCSharp.AbstarctFactoryPattern
         }
     }
 
+     public interface IFurnitureFactory
+    {
+        IChair CreateChair();
+        ISofa CreateSofa();
+        ITable CreateTable();
+    }
+
 }
